40390e7eecaef8439940fcd88528a0a3ef075e44
Azazel3D <fergarpi005@gmail.com>
2026-03-26 13:23:27 -0300

    detecta usuario registrado (no de el archivo)
    unga bunga me gusta hacer fuerza bruta

--- Taller1/src/Paquete/Main.java
+++ Taller1/src/Paquete/Main.java
@@ -16,25 +16,46 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int x = 0;
+        int usuarioCargado = 0;
+
+        String usuarioCorrecto = "";
         
         do {
             System.out.println();
+            
+            if (usuarioCargado == 1) {
+                System.out.println("Bienvenido " + usuarioCorrecto);
+                System.out.println();
 
+            }
             System.out.println("1) Menu de Usuarios");
             System.out.println("2) Menu de Analisis");
             System.out.println("3) Salir");
             System.out.print("> ");
 
             try {
+
                 int opcion = Integer.valueOf(sc.nextLine());
 
                 switch (opcion) {
                     case 1:
-                        System.out.println("Test opcion 1");
+                        System.out.print("Usuario: ");
+                        String usuario = sc.nextLine();
+                        System.out.println("");
+
+                        System.out.print("Contraseña: ");
+                        String contraseña = sc.nextLine();
+                        System.out.println("");
+
+                        usuarioCorrecto = usuario; //hace vo la pega de cargar el usuario nico
+                        usuarioCargado = 1;
+
                         break;
 
                     case 2:
                         System.out.println("Test opcion 2");
+
+
                         break;
 
                     case 3:

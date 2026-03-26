f6d687031eb19a064e3dea785bdfafd409b5e700
Azazel3D <fergarpi005@gmail.com>
2026-03-26 13:11:39 -0300

    test

--- Taller1/src/Paquete/Main.java
+++ Taller1/src/Paquete/Main.java
@@ -14,7 +14,24 @@ public class Main {
         File registros = new File("Taller-Poo/Taller1/src/archivos/Registros.txt");
 
         Scanner sc = new Scanner(System.in);
+
+        int x = 0;
         
+        do {
+            System.out.println();
+
+            System.out.println("1) Menu de Usuarios");
+            System.out.println("2) Menu de Analisis");
+            System.out.println("3) Salir");
+            System.out.print("> ");
+
+            try {
+                int opcion = Integer.valueOf(sc.nextLine());
+
+            } catch (Exception e) {
+                System.out.println("Por favor ingrese un numero valido");
+            }
+        } while (x == 0);
     }
 
 }

d031839a38725f60774ad1c843bb5c158c9cb73f
Azazel3D <fergarpi005@gmail.com>
2026-03-26 13:04:56 -0300

    archivos

--- Taller1/src/Paquete/Main.java
+++ Taller1/src/Paquete/Main.java
@@ -1,5 +1,20 @@
 package Paquete;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File usuarios = new File("Taller-Poo/Taller1/src/archivos/Usuarios.txt");
+
+        File registros = new File("Taller-Poo/Taller1/src/archivos/Registros.txt");
+
+        Scanner sc = new Scanner(System.in);
+        
+    }
 
 }

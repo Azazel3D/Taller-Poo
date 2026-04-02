206bde2a9de7caf69e467f2882b960d716c35990
Azazel3D <fergarpi005@gmail.com>
2026-04-02 20:35:16 -0300

    Arreglo

--- tallerFinal/src/Paquete/Main.java
+++ tallerFinal/src/Paquete/Main.java
@@ -35,9 +35,9 @@ public class Main {
 
 		int a = 0;
 
-        File usuarios = new File("Taller-Poo/Test_1/src/archivos/Usuarios.txt");
+        File usuarios = new File("Taller-Poo/tallerFinal/src/archivos/Usuarios.txt");
 
-        File registros = new File("Taller-Poo/Test_1/src/archivos/Registros.txt");
+        File registros = new File("Taller-Poo/tallerFinal/src/archivos/Registros.txt");
 
         Scanner sc = new Scanner(System.in);
 
@@ -132,9 +132,80 @@ public class Main {
 
 						switch (opcion) {
 
-							case 1: //registrar acticidad | trabajandose
-								
-								System.out.println("Test 1");
+							case 1: //registrar acticidad | Listeke
+
+								int contadorActividades = 0;
+								do {
+									int falso = 0;
+
+									System.out.println();
+									System.out.println("Ingrese la fecha de la actividad (dd/mm/aaaa)");
+									System.out.print("> ");
+									String fechaNueva = sc.nextLine();
+
+									try {
+										String[] partes = fechaNueva.split("/");
+										if (partes.length != 3) {
+											System.out.println("Formato de fecha no válido. Use dd/mm/aaaa.");
+										}else {
+											try {
+												int diaFechaNueva = Integer.valueOf(partes[0]);
+												int mesFechaNueva = Integer.valueOf(partes[1]);
+												int anioFechaNueva = Integer.valueOf(partes[2]);
+
+												if (diaFechaNueva < 1 || diaFechaNueva > 31 || mesFechaNueva < 1 || mesFechaNueva > 12 || anioFechaNueva < 1900) {
+													System.out.println("Fecha no válida. Asegúrese de que el día esté entre 1 y 31, el mes entre 1 y 12 y el año sea mayor a 1900");
+												} else {
+
+													falso++;
+												}
+										}catch (Exception e) {
+											System.out.println("Formato de fecha no válido. Use dd/mm/aaaa.");
+										}
+										}
+									} catch (Exception e) {
+										System.out.println("Error. Intentelo de nuevo");
+									}
+									if (falso == 1) {
+										System.out.println("Ingrese la cantidad de horas en la actividad (En numeros enteros)");
+										System.out.print("> ");
+										try {
+											int horas = Integer.valueOf(sc.nextLine());
+
+											falso++;
+
+											if (falso == 2) {
+												System.out.println("Ingrese la actividad realizada");
+												System.out.print("> ");
+												String actividad = sc.nextLine();
+												
+
+												FileWriter creador = new FileWriter("Taller-Poo/tallerFinal/src/archivos/tempRegistros.txt");
+												Scanner lector = new Scanner(registros);
+												
+												while (lector.hasNextLine()) {
+													String linea = lector.nextLine();
+													creador.write(linea + "\n");
+												}
+												creador.write(usuarioCorrecto + ";" + fechaNueva + ";" + horas + ";" + actividad);
+												creador.close();
+												lector.close();
+												
+												File tempRegistros = new File("Taller-Poo/tallerFinal/src/archivos/tempRegistros.txt");
+
+												if (registros.delete() && tempRegistros.renameTo(registros)) {
+													System.out.println("Actividad registrada con exito!");
+													contadorActividades++;
+												} else {
+													System.out.println("Error al registrar la actividad.");
+												}
+												
+											}
+										} catch (Exception e) {
+											System.out.println("Error. Intentelo de nuevo");
+										}
+									}
+								} while (contadorActividades == 0);
 
 								break;
 
@@ -188,9 +259,92 @@ public class Main {
 								
 								System.out.println("Cual desea eliminar? (Fecha)");
 
-								String fecha = sc.nextLine();
-
+								System.out.print("> ");
 								
+								String fechaEliminar = sc.nextLine();
+
+								Scanner lectorRegistros2 = new Scanner(registros);
+
+								int contador = 0;
+								while (lectorRegistros2.hasNextLine()) {
+									String linea = lectorRegistros2.nextLine();
+									String[] part = linea.split(";");
+
+									if (part[1].equals(fechaEliminar) && part[0].equals(usuarioCorrecto)) {
+										System.out.println(linea);
+										contador++;
+									}
+								}
+
+								if (contador <= 0) {
+									System.out.println("No se encontro la actividad");
+								} else if (contador == 1) {
+									FileWriter creador = new FileWriter("Taller-Poo/tallerFinal/src/archivos/tempRegistros.txt");
+									Scanner lector = new Scanner(registros);
+
+									while (lector.hasNextLine()) {
+										String linea = lector.nextLine();
+										String[] part = linea.split(";");
+
+										if (part[1].equals(fechaEliminar) && part[0].equals(usuarioCorrecto)) {
+											//pass xd
+										} else {
+											creador.write(linea);
+										}
+										if (lector.hasNextLine()) {
+											creador.write("\n");
+										}
+									}
+
+
+									creador.close();
+									lector.close();
+
+									File tempRegistros = new File("Taller-Poo/tallerFinal/src/archivos/tempRegistros.txt");
+
+									if (registros.delete() && tempRegistros.renameTo(registros)) {
+										System.out.println("Actividad eliminada con exito!");
+									} else {
+										System.out.println("Error al eliminar la actividad.");
+									}
+
+								} else {
+										System.out.println("Se enontraron varias actividades con esa fecha, cual desea eliminar? (Escriba la actividad)");
+										System.out.print("> ");
+										String actividadEliminar = sc.nextLine();
+
+
+										FileWriter creador = new FileWriter("Taller-Poo/tallerFinal/src/archivos/tempRegistros.txt");
+									Scanner lector = new Scanner(registros);
+
+									while (lector.hasNextLine()) {
+										String linea = lector.nextLine();
+										String[] part = linea.split(";");
+
+										if (part[1].equals(fechaEliminar)&& part[3].equals(actividadEliminar) && part[0].equals(usuarioCorrecto)) {
+											//pass xd
+										} else {
+											creador.write(linea);
+										}
+										if (lector.hasNextLine()) {
+											creador.write("\n");
+										}
+									}
+
+
+									creador.close();
+									lector.close();
+
+									File tempRegistros = new File("Taller-Poo/tallerFinal/src/archivos/tempRegistros.txt");
+
+									if (registros.delete() && tempRegistros.renameTo(registros)) {
+										System.out.println("Actividad eliminada con exito!");
+									} else {
+										System.out.println("Error al eliminar la actividad.");
+									}
+
+
+									}
 
 
 
@@ -211,7 +365,7 @@ public class Main {
 										String nuevaContra = sc.nextLine();
 
 										
-										FileWriter creador = new FileWriter("Taller-Poo/Test_1/src/archivos/tempUsuarios.txt");
+										FileWriter creador = new FileWriter("Taller-Poo/tallerFinal/src/archivos/tempUsuarios.txt");
 										
 										Scanner lector = new Scanner(usuarios);
 										
@@ -234,7 +388,7 @@ public class Main {
 										creador.close();
 										lector.close();
 										
-										File temp = new File("Taller-Poo/Test_1/src/archivos/tempUsuarios.txt");
+										File temp = new File("Taller-Poo/tallerFinal/src/archivos/tempUsuarios.txt");
 										
 										if (usuarios.delete() && temp.renameTo(usuarios)) {
 
